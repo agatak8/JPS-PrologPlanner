5ba65e734dda10c07ae40c3b691548c58e2800f5
agatak8 <shnutzer@gmail.com>
2018-12-18 13:14:10 +0100

    fix model and write testing procedure

--- model.pl
+++ model.pl
@@ -1,17 +1,50 @@
-InitialState = [on(a, b),
-on(b, c),
-on(c, p1),
-on(d, e),
-on(e, f),
-on(f, g),
-on(g, p2),
-clear(p3),
-clear(p4),
-clear(p5)]
+initialState(
+	[on(a, b),
+	on(b, c),
+	on(c, p1),
+	on(d, e),
+	on(e, f),
+	on(f, g),
+	on(g, p2),
+	clear(a),
+	clear(d),
+	clear(p3),
+	clear(p4),
+	clear(p5)]
+).
 
-goals_achieved(on(Y/on(Y, X/on(X, c))), InitialState).
-goals_achieved(clear(Y), InitialState).
-goals_achieved(clear(A/on(A, e)), InitialState).
-goals_achieved(clear(a), InitialState).
-goals_achieved(on(a, b), InitialState).
-goals_achieved(on(A, b), InitialState).
+testModel :-
+	consult('project.pl'),
+	initialState(State),
+	write("State: "),
+	write_ln(State),
+	write_ln("==============="),
+	GoalLists = [
+		[clear(Y/on(Y, X/on(X, c)))],
+		[clear(Z)],
+		[clear(A/on(A, e))],
+		[clear(a)],
+		[on(a, b)],
+		[on(B, b)],
+		[on(C, b), on(C, c)],
+		[clear(D/on(D,E/on(E,p1)))]
+	],
+	write("Goals: "),
+	write_ln(GoalLists),
+	write_ln("============="),
+	testAll(GoalLists, State).
+
+testAll([], _).
+
+testAll([Goals | Rest], State) :-
+	testGoals(Goals, State),
+	testAll(Rest, State).
+
+testGoals(Goals, State) :-
+	goals_achieved(Goals, State),
+	write_ln(Goals).
+	
+testGoals(Goals, State) :-
+	not(goals_achieved(Goals, State)),
+	write(Goals),
+	write_ln(" not achieved").
