56ef2d79f9e07e08f66ead7b4e149b5b90de4660
agatak8 <shnutzer@gmail.com>
2019-01-19 14:04:52 +0100

    remove debug printing

--- project.pl
+++ project.pl
@@ -1,72 +1,30 @@
 plan_wrapper(InitState, Goals, MaxLimit, Plan, FinalState) :-
 	consult('helpers.pl'),
 	is_between(0, MaxLimit, Limit),
-	write("Trying Limit: "),
-	write_ln(Limit),
 	plan(InitState, Goals, [], Limit, Plan, FinalState, 0).
 
 plan(State, Goals, AchievedGoals, _, [], State, RecursionLevel) :-
-	write("AchievedGoals: "),
-	write_ln(AchievedGoals),
-	goals_achieved(Goals, State),
-	write_ln("Achieved goal").
+	goals_achieved(Goals, State).
 	
 plan(InitState, Goals, AchievedGoals, Limit, Plan, FinalState, RecursionLevel) :-
 	Limit > 0,
-	% write("Recursion: "),
-	% write_ln(RecursionLevel),
-	% wygeneruje LimitPre od 0 do Limit
 	is_between(0, Limit-1, LimitPre),
-	%write("Trying LimitPre: "),
-	%write_ln(LimitPre),
-	choose_goal(Goal, Goals, RestGoals, InitState), % pkt wyboru - kolejnosc goali moze miec znaczenie
+	choose_goal(Goal, Goals, RestGoals, InitState),
 	achieves(Goal, Action),
-	write("Chosen goal: "),
-	write(Goal),
-	write(" needs action: "),
-	write_ln(Action),
 	requires(Action, CondGoals, Conditions),
-	write("Action needs goals: "),
-	write(CondGoals),
-	write(" and conditions: "),
-	write_ln(Conditions),
 	Rec2 is RecursionLevel + 1,
-	write_ln("Making PrePlan to reach action goals..."),
 	plan(InitState, CondGoals, AchievedGoals, LimitPre, PrePlan, State1, Rec2),
-	write("PrePlan: "),
-	write_ln(PrePlan),
-	write("State1: "),
-	write_ln(State1),
-	inst_action(Action, Conditions, State1, InstAction), % pkt wyboru ponownie (miejsca do odstawienia)
-	%check_action(InstAction, AchievedGoals),
-	%write("InstAction is: "),
-	%write_ln(InstAction),
-	%write_ln("Performing action..."),
+	inst_action(Action, Conditions, State1, InstAction),
 	perform_action(State1, InstAction, State2),
-	write("State2: "),
-	write_ln(State2),
-	% reszta idzie do LimitPost
 	LimitPost is Limit - LimitPre,
-	%write("LimitPost is: "),
-	%write_ln(LimitPost),
 	plan(State2, RestGoals, [Goal | AchievedGoals], LimitPost, PostPlan, FinalState, Rec2),
 	conc(PrePlan, [InstAction | PostPlan ], Plan).
-	
-plan(_, _, _, _, _, _) :-
-	write_ln("Failed to find plan within limit"),
-	fail.
 
 goals_achieved([], _).
 
 goals_achieved([HeadGoal | Rest], UnitedState) :-
-	%% member(HeadGoal, UnitedState), %% ! struktura /
 	goal_achieved(HeadGoal, UnitedState),
 	goals_achieved(Rest, UnitedState).
-
-%goal_achieved(A, _) :-
-	%write("Checking if goal achieved: "),
-	%write_ln(A),
-%	fail.
 	
 goal_achieved(on(A, B), UnitedState) :-
 	non_slash(A),
@@ -78,8 +36,6 @@ goal_achieved(clear(A), State) :-
 	member(clear(A), State).
 	
 goal_achieved(clear(A/Goal), State) :-
-	% goal_achieved(clear(X), [...])
-	
 	nonvar(Goal),
 	goal_achieved(Goal, State),
 	member(clear(A), State).
@@ -105,7 +61,6 @@ goal_achieved(on(A/Goal, B/Goal2), UnitedState) :-
 	
 
 choose_goal(Goal, [Goal | Goals], Goals, UnitedState) :-
-	% member(Goal, [Goal | Goals]),
 	not(goal_achieved(Goal, UnitedState)).
 	
 choose_goal(Goal, [X | Goals], [X | TailRestGoals], UnitedState) :-
@@ -114,8 +69,6 @@ choose_goal(Goal, [X | Goals], [X | TailRestGoals], UnitedState) :-
 achieves(on(A, B), move(A, Y/(on(A, Y)), B)).
 
 achieves(clear(B), move(A/on(A,B), B, C)).
-
-%% requires(Action, CondGoals, Conditions) :-CELE
 
 requires(move(What, From, On), [clear(What), clear(On)], [on(What, From)]) :-
 	non_slash(From),
@@ -128,54 +81,22 @@ requires(move(What, From/on(What, From), On), [clear(What), clear(On)], [on(What
 
 requires(move(What, From, On), [clear(What)], [clear(On), safe_diff(On, What)]) :-
 	var(On).
-
-% check_action(InstAction, AchievedGoals)
-check_action(A, G) :-
-    write("Testing if: "),
-    write(A),
-    write(" would destroy goal: "),
-    write_ln(G),
-    fail.
-
+	
 check_action(_, []).
 
 check_action(InstAction, [Goal|Rest]) :-
 	not(action_destroys_goal(InstAction, Goal)),
 	check_action(InstAction, Rest).
 
-check_action(A, G) :-
-    write("Action: ")
-    write(A),
-    write(" would destroy goal: "),
-    write_ln(G),
-    fail.
-
 action_destroys_goal(move(What, From, _), on(What, From)).
 
 action_destroys_goal(move(_, _, On), clear(On)).
 
-
-% inst_action(Action, Conditions, State1, InstAction)
-inst_action(A, CONDS, STATE, _) :-
-	write("inst_action: ACTION: "),
-	write(A),
-	write(" CONDS: "),
-	write(CONDS),
-	write(" STATE: "),
-	write_ln(STATE),
-	fail.
-
 inst_action(move(What, From, On), Conds, UnitedState, move(InstWhat, InstFrom, InstOn)) :-
-	write_ln("Inst action:"),
-	write_ln(What),
-	write_ln(From),
-	write_ln(On),
 	inst_one(What, UnitedState, InstWhat),
 	inst_one(From, UnitedState, InstFrom),
 	inst_one(On, UnitedState, InstOn),
-	conds_achieved(Conds, UnitedState),
-	write("Achieved conditions: "),
-	write_ln(Conds).
+	conds_achieved(Conds, UnitedState).
 
 conds_achieved([], _).
 
@@ -207,11 +128,6 @@ conds_achieved([HeadGoal | Rest], UnitedState) :-
 	HeadGoal \= safe_diff(_, _),
 	goal_achieved(HeadGoal, UnitedState),
 	conds_achieved(Rest, UnitedState).
-
-conds_achieved(D, _) :-
-	write("Failed to achieve conditions: "),
-	write_ln(D),
-	fail.
 
 inst_test(A, US, UI) :-
     nonvar(A),
